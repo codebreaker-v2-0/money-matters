55017031bdfaa2d4de181d669bfa5e9d629a0bdb
ansarahmed <ansarnrml443@gmail.com>
2023-08-24 10:46:08 +0530

    UpdateTransactionBtn Tailwind completed

--- src/components/UpdateTransactionBtn/index.tsx
+++ src/components/UpdateTransactionBtn/index.tsx
@@ -55,12 +55,16 @@ const UpdateTransactionBtn: React.FC<Props> = ({ transaction }) => {
 
   const renderModal = () => (
     <Modal hideModal={hideModal}>
-      <form className={styles.modalContent} onSubmit={onUpdateTransaction}>
+      <form
+        className="min-w-[350px] flex flex-col"
+        onSubmit={onUpdateTransaction}
+      >
         <h3>Update Transaction</h3>
         <ul>
-          <li className={styles.formControl}>
+          <li className="flex flex-col gap-1 my-4 text-left">
             <label htmlFor="transactionName">Transaction Name</label>
             <input
+              className="px-4 py-2 rounded-xl border border-solid border-[#dfeaf2] text-[#718ebf]"
               id="transactionName"
               value={tempTransaction.transactionName}
               onChange={(e) => {
@@ -71,9 +75,10 @@ const UpdateTransactionBtn: React.FC<Props> = ({ transaction }) => {
             />
           </li>
 
-          <li className={styles.formControl}>
+          <li className="flex flex-col gap-1 my-4 text-left">
             <label>Transaction Type</label>
             <select
+              className="bg-white px-4 py-2 rounded-xl border border-solid border-[#dfeaf2] text-[#718ebf]"
               value={tempTransaction.type}
               onChange={(e) => {
                 tempTransaction.type =
@@ -85,9 +90,10 @@ const UpdateTransactionBtn: React.FC<Props> = ({ transaction }) => {
             </select>
           </li>
 
-          <li className={styles.formControl}>
+          <li className="flex flex-col gap-1 my-4 text-left">
             <label>Category</label>
             <select
+              className="bg-white px-4 py-2 rounded-xl border border-solid border-[#dfeaf2] text-[#718ebf]"
               value={tempTransaction.category}
               onChange={(e) => {
                 tempTransaction.category = e.target.value;
@@ -101,9 +107,10 @@ const UpdateTransactionBtn: React.FC<Props> = ({ transaction }) => {
             </select>
           </li>
 
-          <li className={styles.formControl}>
+          <li className="flex flex-col gap-1 my-4 text-left">
             <label htmlFor="amount">Amount</label>
             <input
+              className="px-4 py-2 rounded-xl border border-solid border-[#dfeaf2] text-[#718ebf]"
               id="amount"
               type="number"
               min={1}
@@ -115,9 +122,10 @@ const UpdateTransactionBtn: React.FC<Props> = ({ transaction }) => {
             />
           </li>
 
-          <li className={styles.formControl}>
+          <li className="flex flex-col gap-1 my-4 text-left">
             <label>Date</label>
             <input
+              className="px-4 py-2 rounded-xl border border-solid border-[#dfeaf2] text-[#718ebf]"
               type="date"
               value={tempTransaction.date.slice(0, 10)}
               onChange={(e) =>
@@ -128,7 +136,10 @@ const UpdateTransactionBtn: React.FC<Props> = ({ transaction }) => {
           </li>
         </ul>
 
-        <button className={styles.button} type="submit">
+        <button
+          className="rounded-xl bg-btnPrimaryColor hover:bg-btnPrimaryHoverColor text-white px-4 py-2 active:scale-95"
+          type="submit"
+        >
           Update Transaction
         </button>
       </form>
@@ -138,8 +149,8 @@ const UpdateTransactionBtn: React.FC<Props> = ({ transaction }) => {
   return (
     <>
       {isModalVisible && renderModal()}
-      <button type="button" className={styles.editBtn} onClick={showModal}>
-        <BiPencil className={styles.icon} />
+      <button type="button" className="text-[#2d60ff]" onClick={showModal}>
+        <BiPencil className="m-0 md:mx-4 text-xl" />
       </button>
     </>
   );
