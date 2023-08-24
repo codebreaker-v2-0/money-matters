07b13bed9ac8782f1257421920572ca487e08350
ansarahmed <ansarnrml443@gmail.com>
2023-08-24 10:50:23 +0530

    FailureView Tailwind completed

--- src/components/FailureView/index.tsx
+++ src/components/FailureView/index.tsx
@@ -9,9 +9,11 @@ interface Props {
 }
 
 const FailureView: React.FC<Props> = ({ fetchData }) => (
-  <div className={styles.failureView}>
-    <GiDeathSkull className={styles.icon} />
-    <h3>We are having a hard time loading this page.</h3>
+  <div className="flex flex-col justify-center items-center gap-4 text-[#505887] h-[80vh]">
+    <GiDeathSkull className="text-6xl" />
+    <h3 className="text-normal">
+      We are having a hard time loading this page.
+    </h3>
     <p>Try Again</p>
     <BtnPrimary onClick={fetchData}>Retry</BtnPrimary>
   </div>
