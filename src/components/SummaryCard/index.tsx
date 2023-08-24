733c9ae2f10ebebc69f59bdfe9af1995e4869683
ansarahmed <ansarnrml443@gmail.com>
2023-08-24 12:44:31 +0530

    SummaryCard Tailwind completed

--- src/components/SummaryCard/index.tsx
+++ src/components/SummaryCard/index.tsx
@@ -13,12 +13,24 @@ const SummaryCard: React.FC<Props> = ({ value, type }) => {
       : "https://res.cloudinary.com/dojcknl66/image/upload/v1690638036/debit_e5fy45.png";
 
   return (
-    <div className={styles.summaryCard}>
-      <div className={styles.content}>
-        <p className={styles[type]}>${value.toLocaleString()}</p>
-        <p className={styles.type}>{type === "credit" ? "Credit" : "Debit"}</p>
+    <div className="flex justify-between bg-white p-4 rounded-xl sm:rounded-3xl flex-1 shadow-md">
+      <div className="m-4">
+        <p
+          className={`text-3xl font-semibold ${
+            type === "credit" ? "text-creditColor" : "text-debitColor"
+          }`}
+        >
+          ${value.toLocaleString()}
+        </p>
+        <p className="text-[#718ebf] mt-2">
+          {type === "credit" ? "Credit" : "Debit"}
+        </p>
       </div>
-      <img src={imgUrl} alt={type === "credit" ? "credit" : "debit"} />
+      <img
+        className="max-h-[100px] h-full"
+        src={imgUrl}
+        alt={type === "credit" ? "credit" : "debit"}
+      />
     </div>
   );
 };
